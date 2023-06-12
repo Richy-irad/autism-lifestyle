212feafafd9e53e1495f76e7ce94fbf0543f9cb4
Richy-irad <richardirad@gmail.com>
2023-06-12 13:03:00 +0300

    add button to services when user navigates to checkout without items in cart

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,4 +1,5 @@
 import { FC, useContext, useState } from "react";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { v4 as uuidV4 } from "uuid";
 import Footer from "@/components/footer";
@@ -85,59 +86,71 @@ const Checkout: FC<CheckoutProps> = ({ authToken }) => {
       <main className="font-josefin-sans">
         <div className="px-5 lg:mx-40 py-20">
           <h1 className="text-5xl font-medium">Checkout</h1>
-          <div className="flex justify-between mt-5 items-start">
-            <div className="basis-7/12 flex flex-col gap-y-8">
-              <div className="flex flex-col gap-y-8">
-                <div className="flex gap-x-4">
-                  <h2 className="text-3xl">Billing Details</h2>
+          {cart && cart.items.length ? (
+            <div className="flex justify-between mt-5 items-start">
+              <div className="basis-7/12 flex flex-col gap-y-8">
+                <div className="flex flex-col gap-y-8">
+                  <div className="flex gap-x-4">
+                    <h2 className="text-3xl">Billing Details</h2>
+                    <button
+                      type="button"
+                      className="text-blue text-lg font-bold underline underline-offset-4"
+                      onClick={() => setEditAddress((prevState) => !prevState)}
+                    >
+                      Edit
+                    </button>
+                  </div>
+
+                  <AddressInfo editAddress={editAddress} />
+                </div>
+              </div>
+              <div className="basis-4/12 bg-concrete px-6 py-8 rounded-3xl flex flex-col gap-3">
+                <h3 className="text-2xl font-bold">Summary</h3>
+                <div className="flex flex-col gap-2 text-md">
+                  {cart && cart.items.length && (
+                    <>
+                      {cart.items.map((cartItem, index) => (
+                        <div key={index} className="flex gap-1">
+                          <span className="basis-7/12 flex-grow-1 flex-shrink-1">
+                            {cartItem.item.title}
+                          </span>
+                          <span className="basis-1/12 text-center">
+                            x{cartItem.quantity}
+                          </span>
+                          <span className="basis-4/12 text-end">
+                            KES. {cartItem.subTotal.toLocaleString("en-US")}
+                          </span>
+                        </div>
+                      ))}
+                    </>
+                  )}
+                  <hr className="my-8" />
+                  <div className="flex justify-between font-bold text-base">
+                    <span>Total</span>
+                    <span>KES. {cart.cartTotal.toLocaleString("en-US")}</span>
+                  </div>
+                  <hr className="my-8" />
                   <button
                     type="button"
-                    className="text-blue text-lg font-bold underline underline-offset-4"
-                    onClick={() => setEditAddress((prevState) => !prevState)}
+                    className="bg-primary text-dark text-center font-bold px-4 py-5 rounded-md"
+                    onClick={() => handleSubmitOrderRequest()}
                   >
-                    Edit
+                    Pay KES. {cart.cartTotal.toLocaleString("en-US")}
                   </button>
                 </div>
-
-                <AddressInfo editAddress={editAddress} />
               </div>
             </div>
-            <div className="basis-4/12 bg-concrete px-6 py-8 rounded-3xl flex flex-col gap-3">
-              <h3 className="text-2xl font-bold">Summary</h3>
-              <div className="flex flex-col gap-2 text-md">
-                {cart && cart.items.length && (
-                  <>
-                    {cart.items.map((cartItem, index) => (
-                      <div key={index} className="flex gap-1">
-                        <span className="basis-7/12 flex-grow-1 flex-shrink-1">
-                          {cartItem.item.title}
-                        </span>
-                        <span className="basis-1/12 text-center">
-                          x{cartItem.quantity}
-                        </span>
-                        <span className="basis-4/12 text-end">
-                          KES. {cartItem.subTotal.toLocaleString("en-US")}
-                        </span>
-                      </div>
-                    ))}
-                  </>
-                )}
-                <hr className="my-8" />
-                <div className="flex justify-between font-bold text-base">
-                  <span>Total</span>
-                  <span>KES. {cart.cartTotal.toLocaleString("en-US")}</span>
-                </div>
-                <hr className="my-8" />
-                <button
-                  type="button"
-                  className="bg-primary text-dark text-center font-bold px-4 py-5 rounded-md"
-                  onClick={() => handleSubmitOrderRequest()}
-                >
-                  Pay KES. {cart.cartTotal.toLocaleString("en-US")}
-                </button>
-              </div>
+          ) : (
+            <div className="flex flex-col gap-3 items-start mt-4">
+              <p>You haven't added any packages to your cart to checkout!!!</p>
+              <Link
+                href="/services"
+                className="bg-primary text-dark font-semibold px-4 py-5 rounded-md"
+              >
+                Browse our Services
+              </Link>
             </div>
-          </div>
+          )}
         </div>
       </main>
       <Footer />
