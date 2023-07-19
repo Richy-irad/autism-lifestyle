c1c9dfe20fe5d3f97704e6412dd5fb3c500388ab
Richy-irad <richardirad@gmail.com>
2023-07-19 15:41:48 +0300

    add .env.production for production specific env variables

--- dist/static/index-c3743809-76ead8bc.js
+++
@@ -1,1 +0,0 @@
-import{DocumentPaneProvider as m,DocumentPane as a}from"./index-7415496d-8843ed41.js";import"./sanity-4c37550c.js";import"./json-inspector-72ed92e6.js";export{m as DocumentPaneProvider,a as default};
