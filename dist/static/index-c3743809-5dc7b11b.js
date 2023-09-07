3b36cd5e15a42e2f5c732a8c66ce42d6ec9cf8ff
Richy-irad <richardirad@gmail.com>
2023-09-07 11:21:34 +0300

    deploy sanity

--- dist/static/index-c3743809-5dc7b11b.js
+++
@@ -1,1 +0,0 @@
-import{DocumentPaneProvider as m,DocumentPane as a}from"./index-7415496d-ce5b3504.js";import"./sanity-bd4b3551.js";import"./json-inspector-bae94667.js";export{m as DocumentPaneProvider,a as default};
---
+++ dist/static/index-c3743809-b23af53f.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-7415496d-b026c2b6.js";import"./sanity-4717398c.js";import"./json-inspector-7177b1ff.js";export{m as DocumentPaneProvider,a as default};
